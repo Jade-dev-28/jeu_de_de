a318c18933d7c72fd59a1eea74b9eaa8669dbe24
jade-dev-28 <jade.jsa@gmail.com>
2023-07-03 16:44:03 +0200

    maj

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,24 @@ lancerDe.addEventListener('click', function(){
   des.style.display = 'block';
   console.log('click'); 
 
-  if(nombreAleatoire === 1) {
-    
-
-
-
-
+  if (nombreAleatoire === 1) {
+    current1 = 0; // Réinitialise le score actuel à 0 si le nombre aléatoire est 1
+    current1Div.innerHTML = "Score actuel : " + current1;
+  } else {
+    current1 += nombreAleatoire; // Ajoute le nombre aléatoire au score actuel
+    current1Div.innerHTML = "Score actuel : " + current1;
   }
 
 });
 
+garder.addEventListener('click', function(){
+  score_1 += current1; // Ajoute le score actuel au score global
+  current1 = 0; // Réinitialise le score actuel à 0
+  current1Div.innerHTML = "Score actuel : " + current1;
+  // Mettre à jour l'affichage du score global
+  var score1Div = document.querySelector('.joueur_1 .score_1');
+  score1Div.innerHTML = "Score global : " + score_1;
+});
 
 
  /* function hold() {
