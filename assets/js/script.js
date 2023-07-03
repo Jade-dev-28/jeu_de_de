a254c06ab16661f16f8b647c1b0a491d88327c66
jade-dev-28 <jade.jsa@gmail.com>
2023-07-03 15:54:19 +0200

    ajout page regles et images de

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,35 @@
 var score_1 = 0;
 var current1 = 0;
+var lancerDe = document.getElementById('lancerde');
+var des = document.getElementById('des');
+
+
+lancerDe.addEventListener('click', function(){
+  var nombreAleatoire = Math.floor(Math.random()*6) + 1;
+  des.src = '../images/dice-'+ nombreAleatoire +'.svg';
+  des.style.display = 'block';
+  console.log('click'); 
+
+  if(nombreAleatoire === 1) {
+    
 
 
 
-//function qui me permet de générer un nombre aléatoire entre 1 et 6
-function NombreAleatoire() {
-    var nombreAleatoire = Math.floor(Math.random()*6) + 1;
-    document.querySelector('.compteur').textContent = nombreAleatoire;
-    current1 = nombreAleatoire;
+
   }
 
+});
 
-  
 
-  function hold() {
+
+ /* function hold() {
     score_1 += current1;
     if (score_1 >= 100) {
       alert('Félicitations ! Vous avez atteint un score de 100 ou plus.');
       score_1 = 0;
     }
     document.querySelector('.score1').textContent = score_1;
-  }
-
+  }*/
 
 
   
